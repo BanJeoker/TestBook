823d27402cb2ca45b53297bef5428f19afa0bd03
BanJeoker <38705074+BanJeoker@users.noreply.github.com>
2025-02-21 17:12:50 -0500

    Delete code directory

--- code/tes.py
+++
@@ -1,9 +0,0 @@
-text = "This is the content you want to save."
-
-with open("output.txt", "w", encoding="utf-8") as file:
-    file.write(text)
-
-
-for group_name, group_data in df.groupby('group'):
-    print(f"Group {group_name}: {group_data['category'].tolist()}")
-count_a = df.groupby('group')['category'].apply(lambda x: (x == 'a').sum())
