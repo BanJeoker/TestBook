ee0cd7e095c7563287db6674a980493684afafe6
BanJeoker <38705074+BanJeoker@users.noreply.github.com>
2025-07-23 19:05:58 -0400

    Update tes.py

--- test/tes.py
+++ test/tes.py
@@ -1,1 +1,5 @@
-
+conda create -n myenv python=3.11 -y
+conda activate myenv
+conda install --file "C:\Users\YourName\envs\conda-packages.txt" -y
+python -m ipykernel install --user --name=myenv --display-name "Python (myenv)"
+jupyter notebook
